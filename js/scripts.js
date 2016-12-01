23e8870fd38d9af01778fc47ec13f3f8cac9928c
jday16 <jday16@vcd-ch204-23.eastern.ewu.edu>
2016-12-01 13:43:27 -0800

    multiple cities

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-// My Scripts
+$.simpleWeather({
+    location: '99203',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('img').attr('src', weather.image);
+      $('.title').text(weather.title);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>No Weather :/ </>');
+    }
+  
+  });
