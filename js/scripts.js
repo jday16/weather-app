cd82e20d7202fdd7267f89710b46dba940d54b62
jday16 <janessaday@eagles.ewu.edu>
2016-12-08 22:22:47 -0800

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,47 @@
 // My Scripts
+$.simpleWeather({
+    location: '99004',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.one.temp').text(weather.temp)
+      $('.one.city').text(weather.city)
+     $('.one.state').text(weather.region)
+      $('.one.code').text(weather.region)
+      //change background color
+  $9('body').addClass('bg-'+weather.code)
+      // Entire weather object
+      console.log(weather);
+      console.log(weather.forecast[0].code);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    
+    }
+  
+  });
+
+
+$.simpleWeather({
+    location: '99352',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.two.temp').text(weather.temp)
+      $('.two.city').text(weather.city)
+     $('.two.state').text(weather.region)
+      
+      // Entire weather object
+      console.log(weather);
+      console.log(weather.forecast[0].code);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    
+    }
+  
+  });
